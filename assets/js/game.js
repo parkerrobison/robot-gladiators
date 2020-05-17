97692737680467dc4c5b503fd1ec34b4c7522a20
probison <parker.robison@hotmail.com>
2020-05-16 19:40:19 -0600

    high score I guess

--- assets/js/game.js
+++ assets/js/game.js
@@ -136,13 +136,29 @@ var startGame = function() {
 
 //function endGame()
 var endGame = function() {
-    // if player is still alive, player wins
+    window.alert("The game has now ended. Let's see how you did!");
+
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+        highScore = 0;
+    }
+
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("name" , playerInfo.name);
+        
+        alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+    }
+    else {
+        alert(playerInfo.name + " did not beat the high score of + " + highScore + ". Maybe next time!");
+    }
+    /*// if player is still alive, player wins
     if (playerInfo.health > 0) {
         window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
     }
     else {
         window.alert("You've lost your robot in battle.");
-    }
+    } */
     var playAgainConfirm = window.confirm("Would you like to play again?") 
         if (playAgainConfirm) {
             //restart the game
@@ -151,7 +167,7 @@ var endGame = function() {
         else {
             window.alert("Thank you for playing Robot Gladiators! Come back soon!");
         }
-}
+};
 
 var shop = function () {
     // ask a player what they would like to do
